a9d2dfd8146f62bb876a7b3573cd7b55b98e09dd
Jann Moreno <janncarlomoreno@gmail.com>
2023-06-18 08:24:25 -0500

    Drop App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.jsx'
 import './index.css'
-import './App.css'
 import Main from './components/UI/templates/main'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
