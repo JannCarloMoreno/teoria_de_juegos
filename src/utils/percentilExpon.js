3278e6f1a24deee53a09396cf3fca71a9eb66079
OSarmient <omarcamilo20@gmail.com>
2023-06-17 20:47:19 -0500

    Generador de numeros aleatorios y funcion exponencial

---
+++ src/utils/percentilExpon.js
@@ -0,0 +1,12 @@
+import lcgrand from './lcgrand.js';
+
+function funcionPercentilExpon(media) {
+    /* Función generadora percentil exponencial */
+    /* Retorna una variable aleatoria exponencial con media "media" */
+
+    // Crear un U¬(0,1)
+    let U = lcgrand(Math.floor(Math.random() * 100)); // Elige un índice aleatorio para zrng
+
+    // Nota: En JavaScript, Math.log() retorna el logaritmo natural (base e)
+    return -media * Math.log(U);
+}
