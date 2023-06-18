9e0555fbb2fd65fe1c01e9ba1d7da4c1deef9f19
judsantamariaro <judsantamariaro@unal.edu.co>
2023-06-18 16:15:00 -0500

    tabla realizada estilos arreglados

--- src/components/UI/templates/main/index.jsx
+++ src/components/UI/templates/main/index.jsx
@@ -1,83 +1,121 @@
-import './styles.css'
-import Group from '../../../organisms/group'
-import Prompt from '../../../molecules/prompt'
-import Button from '../../../atoms/button'
-import ShapleyTable from '../../../atoms/table'
-import { useRef, useState } from 'react'
-import {safeParseFloat, calculateAmount, generateBenchs} from '../../../../utils/util'
-import { generateShapleyTable, calculateShapleyForSenate } from '../../../../utils/shapleyFunction'
+import "./styles.css";
+import Group from "../../../organisms/group";
+import Prompt from "../../../molecules/prompt";
+import Button from "../../../atoms/button";
+import ShapleyTable from "../../../atoms/table";
+import { useRef, useState } from "react";
+import {
+  safeParseFloat,
+  calculateAmount,
+  generateBenchs,
+} from "../../../../utils/util";
+import {
+  generateShapleyTable,
+  calculateShapleyForSenate,
+} from "../../../../utils/shapleyFunction";
+import Header from "../../../organisms/header";
+import Simulator from "../../../organisms/simulator";
 
-const getInputData = ref => {
-    const value = ref.current.value
-    let [totalSeats, ...seatsArray]= value.split(',').map((item) => item.trim())
-    seatsArray = seatsArray.filter(item => item.includes('.') && item.length >= 3)
-    seatsArray = seatsArray.map(bench =>calculateAmount(safeParseFloat(bench),totalSeats) );
-    return generateBenchs(seatsArray)
-}
+const getInputData = (ref) => {
+  const value = ref.current.value;
+  let [totalSeats, ...seatsArray] = value.split(",").map((item) => item.trim());
+  seatsArray = seatsArray.filter(
+    (item) => item.includes(".") && item.length >= 3
+  );
+  seatsArray = seatsArray.map((bench) =>
+    calculateAmount(safeParseFloat(bench), totalSeats)
+  );
+  return generateBenchs(seatsArray);
+};
 
-let this_totalSeats = 0
+let this_totalSeats = 0;
 
-const getValues = ref => {
-    const value = ref.current.value
-    let [totalSeats, ...seatsArray]= value.split(',').map((item) => item.trim())
-    seatsArray = seatsArray.filter(item => item.includes('.') && item.length >= 3)
-    seatsArray = seatsArray.map(bench =>safeParseFloat(bench),totalSeats);
-    console.log(totalSeats)
-    this_totalSeats = totalSeats
-    return generateBenchs(seatsArray)
-}
+const getValues = (ref) => {
+  const value = ref.current.value;
+  let [totalSeats, ...seatsArray] = value.split(",").map((item) => item.trim());
+  seatsArray = seatsArray.filter(
+    (item) => item.includes(".") && item.length >= 3
+  );
+  seatsArray = seatsArray.map((bench) => safeParseFloat(bench), totalSeats);
+  console.log(totalSeats);
+  this_totalSeats = totalSeats;
+  return generateBenchs(seatsArray);
+};
 
-export default function Main(){
-    const [totalSeats, setTotalSeats] = useState(0)
-    const [data, setData] = useState(null)
-    const [table, setTable] = useState(null)
-    const [isShowing, setIsShowing] = useState(false)
-    const [values, setValues] = useState(null)
-    
-    const promptRef = useRef(null)
+export default function Main() {
+  const [totalSeats, setTotalSeats] = useState(0);
+  const [data, setData] = useState(null);
+  const [table, setTable] = useState(null);
+  const [isShowing, setIsShowing] = useState(false);
+  const [values, setValues] = useState(null);
 
-    //Cantidad de maquinas requeridas, maximo 100
-    const percentageApproval = 0.50
+  const promptRef = useRef(null);
 
-    let tableButtonText = `${isShowing?'Show':'Hide'} table`
-    
-    const handleClick = () => {
-        const incomingData = getInputData(promptRef)
-        const val = getValues(promptRef)
-        if(JSON.stringify(incomingData)!== JSON.stringify(data)) {
+  //Cantidad de maquinas requeridas, maximo 100
+  const percentageApproval = 0.5;
 
-            //calculate caracterisic function
-            setValues(calculateShapleyForSenate({benches:getValues(promptRef), percentageApproval: percentageApproval}))  
-            setData(incomingData)
-        }
+  let tableButtonText = `${isShowing ? "Show" : "Hide"} table`;
+
+  const handleClick = () => {
+    const incomingData = getInputData(promptRef);
+    const val = getValues(promptRef);
+    if (JSON.stringify(incomingData) !== JSON.stringify(data)) {
+      //calculate caracterisic function
+      setValues(
+        calculateShapleyForSenate({
+          benches: getValues(promptRef),
+          percentageApproval: percentageApproval,
+        })
+      );
+      setData(incomingData);
     }
+  };
 
-    
-    const generateTable = () => {
-        const incomingTable = generateShapleyTable({benches: data, percentageApproval: percentageApproval})
-            if(isShowing){
-                setTable(incomingTable)
-                setIsShowing(false)
-            }else{
-                setTable(null)
-                setIsShowing(true)
-            }
+  const generateTable = () => {
+    const incomingTable = generateShapleyTable({
+      benches: data,
+      percentageApproval: percentageApproval,
+    });
+    if (isShowing) {
+      setTable(incomingTable);
+      setIsShowing(false);
+    } else {
+      setTable(null);
+      setIsShowing(true);
     }
-    console.log(data)
-    
-    return (
-        <section className='main'>
-            <section className='senate'>
-                <section className='prompt'>
-                    <Prompt ref={promptRef} buttonText='Generate' handleChange={handleClick} inputPlaceholder='set configuration' />
-                    {data && <Button className='showButton' onClick={generateTable} text={tableButtonText}/>}
-                </section>
-                <section className='data'>
-                    {data && <Group groups={data} values={values} paidValue={this_totalSeats} /> }
-                    {table && <ShapleyTable data={table}/>}
-                </section>
-            </section>
+  };
+  console.log(data);
+
+  return (
+    <section className="main">
+      <Header />
+      <section className="main__title">
+        <h2>Simulador | Utilidad de recursos computacionales</h2>
+      </section>
+      <Simulator />
+      <section className="senate">
+        <section className="prompt">
+          <Prompt
+            ref={promptRef}
+            buttonText="Generate"
+            handleChange={handleClick}
+            inputPlaceholder="set configuration"
+          />
+          {data && (
+            <Button
+              className="showButton"
+              onClick={generateTable}
+              text={tableButtonText}
+            />
+          )}
         </section>
-
-    )
+        <section className="data">
+          {data && (
+            <Group groups={data} values={values} paidValue={this_totalSeats} />
+          )}
+          {table && <ShapleyTable data={table} />}
+        </section>
+      </section>
+    </section>
+  );
 }
---
+++ src/components/organisms/header/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h3>Simulador | Utilidad de recursos computacionales</h3>
+      <figure>Logo</figure>
+    </header>
+  );
+}
