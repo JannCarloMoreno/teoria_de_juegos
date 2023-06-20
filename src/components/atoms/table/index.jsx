37d80397779214f552e0bff3224cce3597ad4fd9
judsantamariaro <judsantamariaro@unal.edu.co>
2023-06-19 21:29:48 -0500

    style change

--- src/components/atoms/table/index.jsx
+++ src/components/atoms/table/index.jsx
@@ -1,47 +1,72 @@
-import { DataGrid } from '@mui/x-data-grid';
-import './styles.css'
-
+import { DataGrid } from "@mui/x-data-grid";
+import "./styles.css";
 
 const columns = [
-    {field: 'coallitionName', headerName: 'Coaliciones posibles', width: 200, headerAlign: 'center', align: 'center'},
-    {field: 'k', headerName: '# Jugadores', width: 100,headerAlign: 'center', align: 'center'},
-    {field: 'S', headerName: '% Ocupación', width: 100,headerAlign: 'center', align: 'center'},
-    {field: 'vS', headerName: 'Funcion de pagos', width: 200,headerAlign: 'center', align: 'center'},
-    
-]
+  {
+    field: "coallitionName",
+    headerName: "Coaliciones posibles",
+    width: 200,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "k",
+    headerName: "# Jugadores",
+    width: 100,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "S",
+    headerName: "% Ocupación",
+    width: 100,
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "vS",
+    headerName: "Funcion de pagos",
+    width: 200,
+    headerAlign: "center",
+    align: "center",
+  },
+];
 
 const generateRows = (data) => {
-    return Object.keys(data).map((item, index) => {
-        return {
-            id:  index,
-            coallitionName: item,
-            k: data[item].k,
-            S: data[item].S,
-            vS: data[item].vS
-        }
-    })
-}
+  return Object.keys(data).map((item, index) => {
+    return {
+      id: index,
+      coallitionName: item,
+      k: data[item].k,
+      S: data[item].S,
+      vS: data[item].vS,
+    };
+  });
+};
 
-export default function ShapleyTable({data}) {
-    return(
-        <div>
-            <DataGrid 
-                sx = {{
-                    margin: '0 10px',
-                    color: 'white',
-                    backgroundColor: '#3d3d3d',
-                    textAlign: 'center',
-                    height: '100%'
-                }}
-                rows={generateRows(data)}
-                columns ={columns}
-                initialState = {{
-                    pagination: {
-                        paginationModel: {page: 0, pageSize: 8}
-                    }
-                }}
-                pageSizeOptions = {[8,10]}
-            />
-        </div>
-    )
+export default function ShapleyTable({ data }) {
+  return (
+    <div>
+      <DataGrid
+        sx={{
+          margin: "0 10px",
+          color: "white",
+          backgroundColor: "#3d3d3d",
+          textAlign: "center",
+          height: "100%",
+          border: "none",
+          borderRadius: "10px",
+          boxShadow: "2px 3px 10px 0px rgba(0, 0, 0, 0.432)",
+        }}
+        rows={generateRows(data)}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 8 },
+          },
+        }}
+        pageSizeOptions={[8, 10]}
+      />
+    </div>
+  );
 }
--- src/components/organisms/header/index.jsx
+++ src/components/organisms/header/index.jsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <header className="header">
       <h3>Simulador | Utilidad de recursos computacionales</h3>
-      <figure>Logo</figure>
+      <figure>
+        <img src="/src/assets/logoArt.png" alt="logoArt" />
+      </figure>
     </header>
   );
 }
